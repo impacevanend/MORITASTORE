5d3b9510afb1a44f082da76110f8d2bea7365457
Joselo <impacevanend@gmail.com>
2021-09-08 14:28:42 -0500

    Se agrega funcionalidad actulizar más modal

--- php/vistas/articulos.php
+++ php/vistas/articulos.php
@@ -20,9 +20,9 @@ if (isset($_SESSION['usuario'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link rel="stylesheet" href="../css/catalogo.css">
-    <link rel="stylesheet" type="text/css" href="pruebaModal/bootstrap/css/bootstrap.css">
-    <script src="pruebaModal/jquery-3.1.1.min.js"></script>
-    <script src="pruebaModal/bootstrap/js/bootstrap.js"></script>
+        <link rel="stylesheet" type="text/css" href="pruebaModal/bootstrap/css/bootstrap.css">
+        <script src="pruebaModal/jquery-3.1.1.min.js"></script>
+        <script src="pruebaModal/bootstrap/js/bootstrap.js"></script>
         <?php require_once "dependenciasCp.php"; ?>
 
     </head>
@@ -41,13 +41,13 @@ if (isset($_SESSION['usuario'])) {
                 <div class="col-12 col-lg-3 border bg-success">
                     !--*menu Columna izquierda-->
                     <?php require_once("menu.php") ?>
-                    <?php  require_once "../clases/Conexion.php";
-                    
-                        $c = new conectar();
-                        $conexion = $c->conexion();
-                        $sql="SELECT id_categoria, nombreCategoria FROM categorias"; 
+                    <?php require_once "../clases/Conexion.php";
 
-                        $result = mysqli_query($conexion,$sql);
+                    $c = new conectar();
+                    $conexion = $c->conexion();
+                    $sql = "SELECT id_categoria, nombreCategoria FROM categorias";
+
+                    $result = mysqli_query($conexion, $sql);
                     ?>
                 </div>
                 <div class="col-12 col-lg-9 border">
@@ -70,7 +70,7 @@ if (isset($_SESSION['usuario'])) {
                         </div>
 
                     </div>
-                    <div class="container">
+                    <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-4">
 
@@ -79,9 +79,9 @@ if (isset($_SESSION['usuario'])) {
                                         <lable>Categoria</lable>
                                         <select class="from-control input-sm form-label" name="categoriaSelect" id="categoriaSelect">
                                             <option value="A">Elegir Categoría</option>
-                                        <?php while($ver = mysqli_fetch_row($result)):?>
-                                            <option value="<?php echo $ver[0] ?>"><?php echo $ver[1] ?></option>
-                                        <?php endwhile; ?>
+                                            <?php while ($ver = mysqli_fetch_row($result)) : ?>
+                                                <option value="<?php echo $ver[0] ?>"><?php echo $ver[1] ?></option>
+                                            <?php endwhile; ?>
                                         </select><br>
                                     </div>
                                     <div class="mb-3">
@@ -144,6 +144,90 @@ if (isset($_SESSION['usuario'])) {
             </div>
 
         </main>
+        <!-- Modal -->
+        <div class="modal fade" id="abremodalUpdateArticulo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog " role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Actualiza Articulo</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form id="frmArticulosU" enctype="multipart/form-data">
+                            <input type="text" id="idArticulo" hidden="" name="idArticulo">
+                            <label>Categoria</label>
+                            <select class="form-control input-sm" id="categoriaSelectU" name="categoriaSelectU">
+                                <option value="A">Selecciona Categoria</option>
+                                <?php
+                                $sql = "SELECT id_categoria,nombreCategoria
+								from categorias";
+                                $result = mysqli_query($conexion, $sql);
+                                ?>
+                                <?php while ($ver = mysqli_fetch_row($result)) : ?>
+                                    <option value="<?php echo $ver[0] ?>"><?php echo $ver[1]; ?></option>
+                                <?php endwhile; ?>
+                            </select>
+                            <label>Nombre</label>
+                            <input type="text" class="form-control input-sm" id="nombreU" name="nombreU">
+                            <label>Descripcion</label>
+                            <input type="text" class="form-control input-sm" id="descripcionU" name="descripcionU">
+                            <label>Cantidad</label>
+                            <input type="text" class="form-control input-sm" id="cantidadU" name="cantidadU">
+                            <label>Precio</label>
+                            <input type="text" class="form-control input-sm" id="precioU" name="precioU">
+
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button id="btnActualizaarticulo" type="button" class="btn btn-warning" data-dismiss="modal">Actualizar</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </body>
+
+        </html>
+        <script type="text/javascript">
+		function agregaDatosArticulo(idarticulo){
+			$.ajax({
+				type:"POST",
+				data:"idart=" + idarticulo,
+				url:"../procesos/articulos/obtenDatosArticulo.php",
+				success:function(r){
+					
+					dato=jQuery.parseJSON(r);
+					$('#idArticulo').val(dato['id_producto']);
+					$('#categoriaSelectU').val(dato['id_categoria']);
+					$('#nombreU').val(dato['nombre']);
+					$('#descripcionU').val(dato['descripcion']);
+					$('#cantidadU').val(dato['cantidad']);
+					$('#precioU').val(dato['precio']);
+
+				}
+			});
+		}
+
+		function eliminaArticulo(idArticulo){
+			alertify.confirm('¿Desea eliminar este articulo?', function(){ 
+				$.ajax({
+					type:"POST",
+					data:"idarticulo=" + idArticulo,
+					url:"../procesos/articulos/eliminarArticulo.php",
+					success:function(r){
+						if(r==1){
+							$('#tablaArticulosLoad').load("articulos/tablaArticulos.php");
+							alertify.success("Eliminado con exito!!");
+						}else{
+							alertify.error("No se pudo eliminar :(");
+						}
+					}
+				});
+			}, function(){ 
+				alertify.error('Cancelo !')
+			});
+		}
+	</script> 
 
         <script>
             $(document).ready(function() {
@@ -172,11 +256,11 @@ if (isset($_SESSION['usuario'])) {
 
                         success: function(r) {
 
-                          
+
 
                             if (r == 1) {
                                 $('#frmArticulos')[0].reset();
-                                $('#tablaArticulosLoad').load("articulos/tablaArticulos.php"); 
+                                $('#tablaArticulosLoad').load("articulos/tablaArticulos.php");
                                 alertify.success("¡Agregado con exito!");
                             } else {
                                 alertify.error("¡Fallo al subir el archivo!");
@@ -189,9 +273,7 @@ if (isset($_SESSION['usuario'])) {
         </script>
 
         <script src="../js/menu.js"></script>
-    </body>
-
-    </html>
+   
 
 <?php
 } else {
--- php/vistas/articulos/tablaArticulos.php
+++ php/vistas/articulos/tablaArticulos.php
@@ -1,63 +1,63 @@
-
 <?php
-    require_once "../../clases/Conexion.php";
-    $c = new conectar();
-    $conexion = $c->Conexion();
-    $sql = "SELECT art.nombre,
-                    art.descripcion,
-                    art.cantidad,
-                    art.precio,
-                    img.ruta
-                    FROM articulos as art 
-                    inner join imagenes as img
-                    on art.id_imagen=img.id_imagen";
-    $result = mysqli_query($conexion,$sql);
+require_once "../../clases/Conexion.php";
+$c = new conectar();
+$conexion = $c->conexion();
+$sql = "SELECT art.nombre,
+					art.descripcion,
+					art.cantidad,
+					art.precio,
+					img.ruta,
+					cat.nombreCategoria,
+					art.id_producto
+		  from articulos as art 
+		  inner join imagenes as img
+		  on art.id_imagen=img.id_imagen
+		  inner join categorias as cat
+		  on art.id_categoria=cat.id_categoria";
+$result = mysqli_query($conexion, $sql);
 
 ?>
 
-<table class="table table-hover table-condesed " style="text-align: center;">
-                            <thead>
-                                <tr>
-                                    <td>Nombre</td>
-                                    <td>Descripción</td>
-                                    <td>Cantidad</td>
-                                    <td>Precio</td>
-                                    <td>Imagen</td>
-                                    <td>Categoría</td>
-                                    <td>Editar</td>
-                                    <td>Eliminar</td>
-                                </tr>
-                            </thead>
-                                <?php while($ver = mysqli_fetch_row($result) ): ?>
-                                <tbody>
-                                <tr>
-                                    <td><?php echo $ver[0];?></td>
-                                    <td><?php echo $ver[1];?></td>
-                                    <td><?php echo $ver[2];?></td>
-                                    <td><?php echo $ver[3];?></td>
-                                    <td>
-                                        <?php 
-                                        $imgVer = explode("/",$ver[4]);
-                                        $imgruta = $imgVer[1]."/".$imgVer[2]."/".$imgVer[3];
-                                        ?>
-                                        <img width="50" height="50" src="<?php echo $imgruta ?>"
-                                    </td>
-                                    <td></td>
-                                    <td>
-                                            <span class=" btn-sm">
-                                           
-                                            <a href=".." data-toggle="tooltip" title="Edit" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
-                                            </span>
-                                        </td>                                    
-                                        <td>
-                                            <span class=" btn-sm">
+<table class="table table-hover table-condensed table-bordered" style="text-align: center;">
+    <thead>
+        <tr>
+            <td>Nombre</td>
+            <td>Descripción</td>
+            <td>Cantidad</td>
+            <td>Precio</td>
+            <td>Imagen</td>
+            <td>Categoría</td>
+            <td>Editar</td>
+            <td>Eliminar</td>
+        </tr>
+    </thead>
+    <?php while ($ver = mysqli_fetch_row($result)) : ?>
+        <tbody>
+            <tr>
+                <td><?php echo $ver[0]; ?></td>
+                <td><?php echo $ver[1]; ?></td>
+                <td><?php echo $ver[2]; ?></td>
+                <td><?php echo $ver[3]; ?></td>
+                <td>
+                    <?php
+                    $imgVer = explode("/", $ver[4]);
+                    $imgruta = $imgVer[1] . "/" . $imgVer[2] . "/" . $imgVer[3];
+                    ?>
+                    <img width="50" height="50" src="<?php echo $imgruta ?>">
+                <td><?php echo $ver[5]; ?></td>
+                <td>
+                    <span class="btn btn-primary"  data-toggle="modal" data-target="#abremodalUpdateArticulo" class=" btn-sm" onclick="agregaDatosArticulo('<?php echo $ver[6] ?>')">
+                        <a><i class="fa fa-pencil"></i></a>
+                    </span>
+                </td>
+                <td>
+                    <span class=" btn-sm">
 
-                                            <button class="btn btn-success me-md-1" type="button"><i class="far fa-trash-alt"></i></button> 
-                                            
-                                            </span>
-                                        </td> 
-                                </tr>
-                                </tbody>
-                                <?php endwhile; ?>
-                            </table>
-                        
+                        <button class="btn btn-success me-md-1" type="button"><i class="far fa-trash-alt"></i></button>
+
+                    </span>
+                </td>
+            </tr>
+        </tbody>
+    <?php endwhile; ?>
+</table>
